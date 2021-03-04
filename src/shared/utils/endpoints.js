73b796bc1264cffd12485902bf2f414bf13be5be
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-03-04 14:54:11 +0100

    add api structure and local endpoints

---
+++ src/shared/utils/endpoints.js
@@ -0,0 +1,3 @@
+export const loginEndpoint = 'http://localhost:4000/users/login';
+export const createUserEndpoint = 'http://localhost:4000/users/create';
+export const uploadImageEndpoint = 'http://localhost:4000/images/upload';
