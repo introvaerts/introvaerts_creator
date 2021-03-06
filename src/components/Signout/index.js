a333b3adf2baaa887c02fe584957f53059f94ae5
diegog999 <42815611+diegog999@users.noreply.github.com>
2021-03-06 11:33:25 +0100

    implemented private routeto use on Dashboard

--- src/components/Signout/index.js
+++ src/components/Signout/index.js
@@ -1,11 +1,17 @@
+import { useTokenContext } from '../../shared/utils/context.js';
+
 const Signout = () => {
+  //getting token from context
+  const { setToken } = useTokenContext();
+
   //deleting token from local storage
-  const handleLogOut = () => {
+  const handleSignOut = () => {
     localStorage.removeItem('accessToken');
+    setToken(false);
   };
 
   return (
-    <button type="button" onClick={handleLogOut}>
+    <button type="button" onClick={handleSignOut}>
       Log Out
     </button>
   );
--- src/navigation/PrivateRoute.js
+++ src/navigation/PrivateRoute.js
@@ -1,10 +1,14 @@
 import { Redirect, Route } from 'react-router-dom';
+import { useTokenContext } from '../shared/utils/context.js';
+
 function PrivateRoute({ children, ...rest }) {
+  //getting token from context
+  const { token } = useTokenContext();
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        localStorage.getItem('accessToken') ? (
+        token ? (
           children
         ) : (
           <Redirect
