f0692755130feffae1f7bfedd543b3bdcef6a3f4
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-03-08 15:49:34 +0100

    changed to final Domain for api url

--- src/shared/config/api.config.js
+++ src/shared/config/api.config.js
@@ -1,3 +1,1 @@
-/* export const apiBaseURL = 'http://localhost:4000'; */
-export const apiBaseURL =
-  'http://ec2-18-198-203-23.eu-central-1.compute.amazonaws.com/api';
+export const apiBaseURL = 'https://introvaerts.com/api';
