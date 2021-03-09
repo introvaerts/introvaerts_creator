3273e34bd0041472cbd37d30b0c929a19d3cc324
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-03-09 13:11:53 +0100

    implement header section with page_title

--- src/views/Dashboard/Content/index.js
+++ src/views/Dashboard/Content/index.js
@@ -1,11 +1,39 @@
-import { createGalleryEndpoint } from '../../../shared/utils/endpoints';
-
-import FormGallery from '../../../components/FormGallery';
+import { useState } from 'react';
+// shared components
+import SectionContainer from '../../../shared/components/SectionContainer';
+import FormRow from '../../../shared/components/FormRow';
 
 const Content = () => {
+  const [userInput, setUserInput] = useState({
+    page_title: '',
+  });
+
+  const handleSubmit = e => {
+    e.preventDefault();
+  };
+
+  const handleUserInput = e => {
+    const { name, value } = e.target;
+    setUserInput(userInput => ({
+      ...userInput,
+      [name]: value,
+    }));
+    console.log(userInput);
+  };
+
   return (
-    <form>
-      <h2>Content</h2>
+    <form method="POST" onSubmit={handleSubmit}>
+      <SectionContainer>
+        <FormRow
+          type="text"
+          name="page_title"
+          label="page title"
+          id="page_title"
+          value={userInput.page_title}
+          required
+          handleChange={handleUserInput}
+        />
+      </SectionContainer>
     </form>
   );
 };
