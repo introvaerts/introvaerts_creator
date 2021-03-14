4c1ceec3c7028d91fb75aedeab514c9dfbcf0d36
Jasmin Jenull <jasminjenull@gmail.com>
2021-03-14 18:51:20 +0100

    Add route for image upload

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,12 +1,6 @@
 /* import DashboardRouter from "../../navigation/DashboardRouter"; */
 import { useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Link,
-  useRouteMatch,
-  Switch,
-  Route,
-} from 'react-router-dom';
+import { Link, useRouteMatch, Switch, Route } from 'react-router-dom';
 // api
 import Api from '../../shared/utils/api';
 //global token
@@ -19,12 +13,12 @@ import Settings from './Settings';
 import Signout from '../../components/Signout';
 import Logo from '../../components/Logo';
 import Gallery from './Gallery';
+import Image from './Image/index';
 // import Menu from '../../components/Menu';
 
 import {
   MenuContainer,
   MenuItem,
-  Dropdown,
   SignOutBlock,
   LoggedInUser,
   MenuBox,
@@ -84,56 +78,59 @@ const Dashboard = () => {
 
   return (
     <div>
-      <Router>
-        <MenuContainer>
-          <MenuBox>
-            <Logo />
-            <MenuItem>
-              <Link to={`${url}/design`}>
-                <h3>Design</h3>
-              </Link>
-              {/* <Dropdown>
+      {/* <Router> */}
+      <MenuContainer>
+        <MenuBox>
+          <Logo />
+          <MenuItem>
+            <Link to={`${url}/design`}>
+              <h3>Design</h3>
+            </Link>
+            {/* <Dropdown>
                 <h3>Colours</h3>
                 <h3>Typography</h3>
               </Dropdown> */}
-            </MenuItem>
-            <MenuItem>
-              <Link to={`${url}/content`}>
-                <h3>Content</h3>
-              </Link>
-              {/* <Dropdown>
+          </MenuItem>
+          <MenuItem>
+            <Link to={`${url}/content`}>
+              <h3>Content</h3>
+            </Link>
+            {/* <Dropdown>
                 <h3>Header</h3>
                 <h3>About</h3>
                 <h3>Gallery</h3>
                 <h3>Contact</h3>
               </Dropdown> */}
-            </MenuItem>
-            <MenuItem>
-              <Link to={`${url}/settings`}>
-                <h3>Settings</h3>
-              </Link>
-            </MenuItem>
-          </MenuBox>
-          <SignOutBlock>
-            {logOutButton()}
-            <LoggedInUser>Welcome [username] !</LoggedInUser>
-          </SignOutBlock>
-        </MenuContainer>
+          </MenuItem>
+          <MenuItem>
+            <Link to={`${url}/settings`}>
+              <h3>Settings</h3>
+            </Link>
+          </MenuItem>
+        </MenuBox>
+        <SignOutBlock>
+          {logOutButton()}
+          <LoggedInUser>Welcome [username] !</LoggedInUser>
+        </SignOutBlock>
+      </MenuContainer>
 
-        {/* <DashboardRouter /> */}
-        <Switch>
-          <Route exact path={path} />
-          <Route path={`${path}/design`} component={Design} />
-          <Route path={`${path}/content`}>
-            {/* uses the only on subdomain of the user */}
-            <Content subdomain={userInfo ? userInfo.subdomains[0] : null} />
-          </Route>
-          <Route path={`${path}/galleries/:name`}>
-            <Gallery />
-          </Route>
-          <Route path={`${path}/settings`} component={Settings} />
-        </Switch>
-      </Router>
+      {/* <DashboardRouter /> */}
+      <Switch>
+        <Route exact path={path} />
+        <Route path={`${path}/design`} component={Design} />
+        <Route path={`${path}/content`}>
+          {/* uses the only on subdomain of the user */}
+          <Content subdomain={userInfo ? userInfo.subdomains[0] : null} />
+        </Route>
+        <Route path={`${path}/galleries/:name/image-upload`}>
+          <Image />
+        </Route>
+        <Route path={`${path}/galleries/:name`}>
+          <Gallery />
+        </Route>
+        <Route path={`${path}/settings`} component={Settings} />
+      </Switch>
+      {/* </Router> */}
     </div>
   );
 };
