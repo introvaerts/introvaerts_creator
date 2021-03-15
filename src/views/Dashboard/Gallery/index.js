4d8b1eb59cb6c77592a77c0fc1384a7839b8acce
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-03-15 13:08:56 +0100

    prepare for passing images to grid

--- src/views/Dashboard/Gallery/index.js
+++ src/views/Dashboard/Gallery/index.js
@@ -1,4 +1,13 @@
 import { Link, useParams } from 'react-router-dom';
+import ImageGrid from '../../../components/ImageGrid';
+
+// call API and get images of this galleryId and pass them to ImageGrid
+const images = [
+  'https://picsum.photos/300/200',
+  'https://picsum.photos/200/140',
+  'https://picsum.photos/100/30',
+  'https://picsum.photos/200/400',
+];
 
 const Gallery = () => {
   const { name } = useParams();
@@ -6,6 +15,7 @@ const Gallery = () => {
     <div>
       Gallery: {name}
       <Link to={`/dashboard/galleries/${name}/image-upload`}>Add image</Link>
+      <ImageGrid images={images} />
     </div>
   );
 };
