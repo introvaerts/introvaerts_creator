b4c1dc61299a58a0808cfc7c79dffe69a8795f97
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-03-16 21:40:47 +0100

    indentation

--- src/navigation/MainRouter.js
+++ src/navigation/MainRouter.js
@@ -1,24 +1,22 @@
 import { Switch, Route, Redirect } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute.js';
 
-import { ROOT, SIGNIN, SIGNUP, SIGNOUT, DASHBOARD } from "./CONSTANTS";
+import { ROOT, SIGNIN, SIGNUP, SIGNOUT, DASHBOARD } from './CONSTANTS';
 
-import Signin from "../components/Signin";
-import Signup from "../components/Signup";
-import Dashboard from "../views/Dashboard";
+import Signin from '../components/Signin';
+import Signup from '../components/Signup';
+import Dashboard from '../views/Dashboard';
 
-
-const MainRouter = ({refreshApp}) => (
-    <Switch>
-      <Redirect exact from={ROOT} to={SIGNIN} />
-      <Route exact path={SIGNIN} component={Signin} />
-      <Route exact path={SIGNUP} component={Signup} />
-      <Route exact path={SIGNOUT} component={SIGNOUT} />
-      <PrivateRoute  path={DASHBOARD}>
-        <Dashboard />
-      </PrivateRoute>
-    </Switch>
-
+const MainRouter = ({ refreshApp }) => (
+  <Switch>
+    <Redirect exact from={ROOT} to={SIGNIN} />
+    <Route exact path={SIGNIN} component={Signin} />
+    <Route exact path={SIGNUP} component={Signup} />
+    <Route exact path={SIGNOUT} component={SIGNOUT} />
+    <PrivateRoute path={DASHBOARD}>
+      <Dashboard />
+    </PrivateRoute>
+  </Switch>
 );
 
 export default MainRouter;
