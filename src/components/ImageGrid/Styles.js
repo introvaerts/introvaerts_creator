a39bfe7b3509426758927e031a24f0d9e2672212
diegog999 <42815611+diegog999@users.noreply.github.com>
2021-03-17 16:34:45 +0100

    more grid styling

--- src/components/ImageGrid/Styles.js
+++ src/components/ImageGrid/Styles.js
@@ -4,4 +4,6 @@ export const StyledGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(18vw, 1fr));
   grid-gap: 1rem;
+  width: 87vw;
+  margin: 0 auto;
 `;
--- src/shared/components/Thumbnail/Styles.js
+++ src/shared/components/Thumbnail/Styles.js
@@ -2,8 +2,15 @@ import styled from 'styled-components';
 
 export const ImageContainer = styled.div`
   position: relative;
+  display: grid;
   &:hover {
     opacity: 0.8;
+  }
+  &::before {
+    content: '';
+    padding-bottom: 100%;
+    display: block;
+    grid-area: 1 / 1 / 2 / 2;
   }
 `;
 
@@ -11,5 +18,5 @@ export const Image = styled.img`
   object-fit: cover;
   object-position: center;
   width: 100%;
-  ${'' /* grid-area: 1 / 1 / 2 / 2; */}
+  grid-area: 1 / 1 / 2 / 2;
 `;
