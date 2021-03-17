f71647d8780a4ee5d3f2ba4eefa68544f33f0b11
diegog999 <42815611+diegog999@users.noreply.github.com>
2021-03-17 16:36:31 +0100

    adjusted column size

--- src/components/ImageGrid/Styles.js
+++ src/components/ImageGrid/Styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledGrid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(18vw, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(15vw, 1fr));
   grid-gap: 1rem;
   width: 87vw;
   margin: 0 auto;
