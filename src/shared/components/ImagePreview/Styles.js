d68e1ef0a4b794179584e7414ad16467c2558bf7
Janis Jeanne Merkel <janisjeanne.merkel@gmail.com>
2021-03-17 12:12:11 +0100

    fix size of image in preview componen

--- src/shared/components/ImagePreview/Styles.js
+++ src/shared/components/ImagePreview/Styles.js
@@ -7,9 +7,15 @@ export const PreviewContainer = styled.div`
   left: ${props => props.left}%;
   max-width: ${props => props.maxWidth}vw;
   max-height: 80%;
+  width: 15vw;
+  height: 15vw;
 `;
 
 export const StyledImage = styled.img`
+  ${'' /* width: 100%;
+  height: auto; */}
+  object-fit: cover;
+  object-position: center;
+  height: 100%;
   width: 100%;
-  height: auto;
 `;
